eb61d5bd74bba98eb93f9d857e5c1635b1988dcd
Georghios Skettos <georghios.skettos@here.com>
2016-04-11 11:11:56 +0200

    Change places env to production

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ var here = window.here || {};
 here.builder = {
     config: {
         shareURL: 'https://share.here.com/',
-        PBAPI: "https://places.sit.api.here.com/places/v1/",
+        PBAPI: "https://places.api.here.com/places/v1/",
         appId: '45vC3JHWu5fUqqUm9Ik2',
         appCode: 'U8WhuCuhmYAHgttfjOEdfg'
     }
